847ecc20d2d3e25c4376b6b134c2a58e21a299cc
tonyxuan2021 <tonyyexuan@gmail.com>
2023-02-24 15:21:18 -0800

    setting up FE and fetched data from graphql server

---
+++ client/index.js
@@ -0,0 +1,20 @@
+import './style/style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import SongList from './components/SongList';
+
+const client = new ApolloClient({
+  dataIdFromObject: (o) => o.id,
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <SongList />
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
