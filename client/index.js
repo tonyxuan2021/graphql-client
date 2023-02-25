f18d6d9c5b2c10e453aec49c34fc360e6f733207
tonyxuan2021 <tonyyexuan@gmail.com>
2023-02-24 16:37:54 -0800

    added react router dom

--- client/index.js
+++ client/index.js
@@ -4,15 +4,23 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import SongList from './components/SongList';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import App from './components/App';
+import SongCreate from './components/SongCreate';
 
 const client = new ApolloClient({
-  dataIdFromObject: (o) => o.id,
+  // dataIdFromObject: (o) => o.id,
 });
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList}></IndexRoute>
+          <Route path="song/new" component={SongCreate}></Route>
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
