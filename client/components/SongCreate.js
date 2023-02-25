4dda650e686e3dccf2f1cbd4bc03b306d53f9c41
tonyxuan2021 <tonyyexuan@gmail.com>
2023-02-24 20:59:19 -0800

    added ability to allow write data using gql server

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { title: '' };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        title: this.state.title,
+      },
+    });
+    // console.log(this.props);
+  }
+
   render() {
     return (
       <div>
         <h3>Create a New Song</h3>
-        <form></form>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Song Title:</label>
+          <input
+            onChange={(event) =>
+              this.setState({
+                title: event.target.value,
+              })
+            }
+            value={this.state.title}
+          />
+        </form>
       </div>
     );
   }
 }
 
-export default SongCreate;
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);
